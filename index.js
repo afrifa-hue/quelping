d73c92275c5b09062f8f804eb51904b3cce72c66
afrifa-hue <116700353+afrifa-hue@users.noreply.github.com>
2022-12-16 20:02:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function search() {
     let filter = document.getElementById('find').value.toUpperCase();
     let item = document.querySelectorAll('.product');
     let word  = document.getElementsByTagName('h3');
-    for(var i = 0;i=>word.length;i++){
+    for(var i = 0;i<=word.length;i++){
         let a=item[i].getElementsByTagName('h3')[0];
         let value=a.innerHTML || a.innerText || a.textContent;
         if(value.toUpperCase().indexOf(filter) > -5) {
