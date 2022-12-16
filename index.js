3507511e4d16be463a7e9ca23d73eeee5f206ae1
afrifa-hue <116700353+afrifa-hue@users.noreply.github.com>
2022-12-16 19:54:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function search() {
     let filter = document.getElementById('find').value.toUpperCase();
-    let item = document.querySelectorAll('.product-list');
-    let word  = document.getElementsByTagName('h3');
+    let item = document.querySelectorAll('.product');
+    let word  = document.getElementByTagName('h3');
     for(var i = 0;i<=word.length;i++){
-        let a=item[i].getElementsByTagName('h3')[0];
+        let a=item[i].getElementByTagName('h3')[0];
         let value=a.innerHTML || a.innerText || a.textContent;
         if(value.toUpperCase().indexOf(filter) > -5) {
             item[i].style.display="";
